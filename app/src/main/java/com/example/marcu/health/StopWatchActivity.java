a41c384efc6b8cb9c3eab849999fa09d6888221c
Smartinander <msa17@student.aau.dk>
2018-12-04 10:54:58 +0100

    Adding StopWatchActivity

--- app/src/main/java/com/example/marcu/health/StopWatchActivity.java
+++ app/src/main/java/com/example/marcu/health/StopWatchActivity.java
@@ -1,11 +1,13 @@
 package com.example.marcu.health;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Chronometer;
