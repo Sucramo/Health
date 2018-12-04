34a63c6204375ac16e4917fd02dc2141fb805d91
Smartinander <msa17@student.aau.dk>
2018-12-04 10:36:13 +0100

    Profile and History java classes

---
+++ app/src/main/java/com/example/marcu/health/ProfileActivity.java
@@ -0,0 +1,47 @@
+package com.example.marcu.health;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class ProfileActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_layout);
+
+
+        //Bottomnavigation stuff
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_profile:
+                        Intent intent1 = new Intent(getApplicationContext(), ProfileActivity.class);
+                            startActivity(intent1.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION));
+                        break;
+                    case R.id.action_tracking:
+                        Intent intent2 = new Intent(getApplicationContext(), StopWatchActivity.class);
+                        startActivity(intent2.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION));
+                        break;
+                    case R.id.action_history:
+                        Intent intent3 = new Intent(getApplicationContext(), HistoryActivity.class);
+                        startActivity(intent3.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION));
+                        break;
+
+                }
+                return true;
+            }
+        });
+
+
+    }
+
+}
