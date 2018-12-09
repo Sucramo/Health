dc953b49f467832397e7f587fe6f8a17c19528b7
Smartinander <msa17@student.aau.dk>
2018-12-09 16:58:59 +0100

    Profile and History java classes

--- app/src/main/java/com/example/marcu/health/StopWatchActivity.java
+++ app/src/main/java/com/example/marcu/health/StopWatchActivity.java
@@ -1,11 +1,14 @@
 package com.example.marcu.health;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Chronometer;
@@ -38,9 +41,8 @@ public class StopWatchActivity extends AppCompatActivity {
         chronometer = findViewById(R.id.chronometer);
 
 
-
         //Bottomnavigation stuff
-        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
         buttonStartOne = findViewById(R.id.start_button_one);
         buttonStartTwo = findViewById(R.id.start_button_two);
@@ -72,6 +74,8 @@ public class StopWatchActivity extends AppCompatActivity {
             }
         });
     }
+
+
 
     private void initDataToSeekbar() {
         ArrayList<ProgressItem> progressItemList = new ArrayList<>();
@@ -150,8 +154,8 @@ public class StopWatchActivity extends AppCompatActivity {
         buttonSave.setVisibility(View.INVISIBLE);
 
         String StringACWR = df.format(Double.valueOf(ACWR));
-        if(ACWR == 0){
-            String daysLeft = Integer.toString(28-al.size());
+        if (ACWR == 0) {
+            String daysLeft = Integer.toString(28 - al.size());
             textViewACWR.setText(daysLeft + " days left");
         } else {
             textViewACWR.setText(StringACWR);
@@ -166,8 +170,8 @@ public class StopWatchActivity extends AppCompatActivity {
         ACWR = mathActivity.getACWR(getRandomMinutes(), getRandomHR(), al);
 
         String StringACWR = df.format(Double.valueOf(ACWR));
-        if(ACWR == 0){
-            String daysLeft = Integer.toString(28-al.size());
+        if (ACWR == 0) {
+            String daysLeft = Integer.toString(28 - al.size());
             textViewACWR.setText(daysLeft + " days left");
         } else {
             textViewACWR.setText(StringACWR);
